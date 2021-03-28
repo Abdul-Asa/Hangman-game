05f58866a3e2432e549173c8a20e318272854eb3
Abdullah Shehu <55896361+Abdul-Asa@users.noreply.github.com>
2021-03-28 11:44:39 +0100

    Just checking my git

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
-myDisplay = document.querySelector("#display");
-userInput = document.querySelector("#userIn");
-myButton = document.querySelector("#check");
-myComment = document.querySelector("#comment")
-//Query selector of all the fields
+var myDisplay = document.querySelector("#display");
+var userInput = document.querySelector("#userIn");
+var myButton = document.querySelector("#check");
+var myComment = document.querySelector("#comment")
+var tryA = document.querySelector("#tryAgain")
 
 var meals = ["rice","amala","indomie","fufu",];
-var randomPick = Math.floor(Math.random()*meals.length);
-var answer = meals[randomPick];
 var question = [];
-//initialize all the variables used
-
-for(let i=0; i<answer.length;i++){
-    question.push("_");
-}
-//Use a loop to create the dashes
-
-myDisplay.innerHTML = question.join(" ");
-console.log(answer); 
-
-function checkAnswer(){
-    var bool = false;
+var loop = 0;
+var answer;
+var TA = true;
+while(TA == true){
+    answer = meals[loop];
     for(let i=0; i<answer.length;i++){
-        if(userInput.value == answer[i]){
-            bool = true;
-            question[i] = answer[i];
-            //If it matches, it removes the dash and replaces it with the right answer
-        }
+        question.push("_");
     }
 
     myDisplay.innerHTML = question.join(" ");
-    if (bool == true) {
-        myComment.innerHTML = "Correct"
-        
-    } else {
-        myComment.innerHTML = "Wrong"
+    console.log(answer); 
+
+    function checkAnswer(){
+        var bool = false;
+        for(let i=0; i<answer.length;i++){
+            if(userInput.value == answer[i]){
+                bool = true;
+                question[i] = answer[i];
+            }
+        }
+
+        myDisplay.innerHTML = question.join(" ");
+        if (bool == true) {
+            myComment.innerHTML = "Correct"
+            
+        } else {
+            myComment.innerHTML = "Wrong"
+        }
+
     }
 
-}
 
-myButton.addEventListener("click",checkAnswer);
+
+    myButton.addEventListener("click",checkAnswer);
+}
