54d3471ef588bc360ff30a597492193ab996b5eb
Abdullah Shehu <55896361+Abdul-Asa@users.noreply.github.com>
2021-04-01 07:15:56 +0100

    Added a lives counter and display

--- script.js
+++ script.js
@@ -7,21 +7,33 @@ var container2 = document.querySelector(".container2");
 var container3 = document.querySelector(".container3");
 var nextButton = document.querySelector("#nextQ");
 var exitButton = document.querySelector("#bye");
+var buttonPad = document.querySelector(".buttons");
+var playerLife = document.querySelector("#liveBar")
 
 container2.style.display="none";
 container3.style.display="none";
 
-var meals = ["SPIDERMAN","SUPERMAN","HULK","HAWKEYE","FLASH"];
+var meals = ["FRANCE","NIGERIA","JAPAN","BRAZIL","CANADA","MEXICO","GERMANY"];
 var question ;
 var loop = 0;
 var answer;
+var lives = 5;  
+var str;
+    
 
 function playGame(){ 
+    // answer = meals[Math.floor(Math.random()*meals.length)];
+    for (let i = 0; i < lives; i++) {
+        str = str + "&#9829 ";
+        playerLife.innerHTML = "Chances : " + str;
+    }
     answer = meals[loop];
     myComment.style.display="none";
     tryA.style.display="none";
     container2.style.display="none";
-    container3.style.display="none";    
+    container3.style.display="none"; 
+    buttonPad.style.display="grid"; 
+    nextButton.innerHTML="Next";  
     question=[];
    
     for(let i=0; i < answer.length;i++){
@@ -32,6 +44,7 @@ function playGame(){
 }
 
 function checkAnswer(u){
+    str = "";
     var bool = false;
     var completeWord = true;
     for(let i=0; i<answer.length;i++){
@@ -48,8 +61,26 @@ function checkAnswer(u){
         
     } else {
         myComment.style.display="block";
-        myComment.innerHTML = "Wrong"
+        myComment.innerHTML = "Wrong";
+        lives--;
+        for (let i = 0; i < lives; i++) {
+            str = str + "&#9829 ";
+            playerLife.innerHTML = "Chances : " + str;
+        }
+
     }
+    
+    if(lives==0){
+        lives=5;
+        buttonPad.style.display="none";
+        tryA.style.display="block";
+        tryA.innerHTML = "Failed 😔";
+        playerLife.innerHTML = "&#128148"
+        container2.style.display="block";
+        nextButton.innerHTML="Try again";
+    }
+    // console.log(lives)
+    
     for(let i=0; i<answer.length;i++){
         if(question[i] == "_"){
             completeWord = false; 
@@ -65,6 +96,7 @@ function checkAnswer(u){
 
         }
     }
+
 
 } 
 
